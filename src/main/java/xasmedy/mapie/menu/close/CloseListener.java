3d5f9bc0eb2fdf8c63f36b8ac7c9171f1e4c8e4b
xasmedy <xasmedy08cim@gmail.com>
2023-03-29 23:17:29 +0200

    Added copyright.

--- src/main/java/xasmedy/mapie/menu/close/CloseListener.java
+++ src/main/java/xasmedy/mapie/menu/close/CloseListener.java
@@ -1,3 +1,11 @@
+/*
+ * Copyright (c) 2023 - Xasmedy.
+ * This file is part of the Mindustry API Extension Project licensed under GNU-GPLv3.
+ *
+ * The Project source-code can be found at https://github.com/Xasmedy/MAPIE
+ * Contributors of this file may put their name into the copyright notice.
+ */
+
 package xasmedy.mapie.menu.close;
 
 import xasmedy.mapie.menu.Menu;
